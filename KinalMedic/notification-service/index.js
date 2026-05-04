fb2b37c3163f41140dd860e6839c3a4d220171bb
dlucas-2024332 <dlucas-2024332@kinal.edu.gt>
2026-05-04 00:47:19 -0600

    feat: Se añadio la implementación de la documentación de swagger al servicio notification-service y la modificación de rutas para la funcionalidad de swagger en este servicio de enviar una petición de salud.

--- KinalMedic/notification-service/index.js
+++ KinalMedic/notification-service/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import notificationRoutes from './src/routes/notification.routes.js';
+import { swaggerDocs, swaggerUi } from './config/documentation.js';
 
 dotenv.config();
 
@@ -10,9 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/api/notifications', notificationRoutes);
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
     console.log(`Notification Service (ESM) running on port ${PORT}`);
+    console.log(`Swagger: http://localhost:${PORT}/api-docs`);
 });
--- KinalMedic/notification-service/src/routes/notification.routes.js
+++ KinalMedic/notification-service/src/routes/notification.routes.js
@@ -1,10 +1,34 @@
 import { Router } from 'express';
-
 import { requestMedicalHelp } from '../controller/notification.controller.js';
 import { validateNotification } from '../../middlewares/validate-request.js';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/notifications/request-help:
+ *   post:
+ *     summary: Enviar una alerta médica inmediata
+ *     description: Envía un correo electrónico estructurado al médico especificado con los detalles del estudiante y la urgencia.
+ *     tags: [Notifications]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NotificationInput'
+ *     responses:
+ *       200:
+ *         description: Correo enviado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/NotificationResponse'
+ *       400:
+ *         description: Error en la validación de los datos (middleware)
+ *       500:
+ *         description: Error interno al intentar enviar el correo (Nodemailer)
+ */
 router.post('/request-help', [validateNotification], requestMedicalHelp);
 
 export default router;
