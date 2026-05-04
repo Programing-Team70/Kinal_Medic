b059af41cb2fcbb52e783f4d5bdb5501114c9178
dlucas-2024332 <dlucas-2024332@kinal.edu.gt>
2026-05-04 00:34:09 -0600

    feat: Se añadio la implementación de la documentación de swagger al servicio medical-records-service y la modificación de rutas para la funcionalidad de swagger en este servicio de registros.

--- KinalMedic/medical-records-service/index.js
+++ KinalMedic/medical-records-service/index.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import recordRoutes from './src/routes/record.routes.js';
 import connectDB from './config/db.js';
+import { swaggerDocs, swaggerUi } from './config/documentation.js';
 
 const app = express();
 
@@ -11,9 +12,12 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/api/records', recordRoutes);
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Servicio Médico escuchando en el puerto ${PORT}`);
+    console.log(`Swagger: http://localhost:${PORT}/api-docs`);
 });
